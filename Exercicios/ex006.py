f879f15f1a66ecae17866fd9e760b63c9d357193
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-05-07 18:21:28 -0300

    Dobro, Triplo e Raiz Quadrada
    Crie um algoritmo que leia um número e mostre seu dobro, triplo e raiz quadrada

---
+++ Exercicios/ex006.py
@@ -0,0 +1,11 @@
+valor = int(input('Digite um número: '))
+
+dobro = valor * 2
+triplo = valor * 3
+raiz_quadrada = valor ** 0.5
+
+print(f'O dobro de {valor} vale {dobro}.\n'
+      f'O triplo de {valor} vale {triplo}. \n'
+      f'A raiz quadrada de {valor} é igual a {raiz_quadrada:.2f}.' 
+      ) 
+
