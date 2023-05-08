eb61b0c5386b74959155a798e45f71effe58f29e
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-05-08 14:30:12 -0300

    Média Aritmética
    Desenvolva um programa que leia duas notas de um aluno,  calcule e mostre a sua média.

---
+++ Exercicios/ex007.py
@@ -0,0 +1,7 @@
+nota1 = float(input('Digite a primeira nota do aluno: '))
+nota2 = float(input('Digite a segunda nota do aluno: '))
+
+media = (nota1 + nota2) / 2
+
+print(f'A média entre as notas {nota1} e {nota2} é igual a: {media:.1f}')
+
