b27c555115d7527113234cd713d97b5a9d36e146
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-05-09 17:22:01 -0300

    Conversor de Medidas
    Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.

---
+++ Exercicios/ex008.py
@@ -0,0 +1,12 @@
+medida = float(input('Informe ama distância em metros: '))
+
+dm = medida * 10
+cm = medida * 100
+mm = medida * 1000
+dam = medida / 10
+hm = medida / 100
+km = medida / 1000
+
+print(f'A medida de {medida} metros, corresponde a:\n'
+      f'{km} quilômetros, {hm} hectômetros, {dam} decâmetros, \n'
+      f'{dm} decímetros, {cm} centímetros, {mm} milímetros')
