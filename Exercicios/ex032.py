dccf919b06bb05abb5055c085f04cd01a968b500
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-05-31 10:26:52 -0300

    Ano Bissexto
    Faça um programa que leia um ano qualquer e mostre se ele é bissexto.

--- Exercicios/ex032.py
+++ Exercicios/ex032.py
@@ -7,4 +7,4 @@ if ano == 0:
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print(f'O ano {ano} é BISSEXTO')
 else:
-    print(f'O ano {ano} NÃO é Bissexto')
+    print(f'O ano {ano} NÃO é BISSEXTO')
