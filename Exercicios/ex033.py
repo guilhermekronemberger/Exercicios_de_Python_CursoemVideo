5c74cd5ec2a4e1bfdad8056e31471f64a104e9c9
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-06-01 16:12:06 -0300

    Maior e menor valor
    Crie um programa que leia três números e mostre qual é o maior e qual é o menor.

---
+++ Exercicios/ex033.py
@@ -0,0 +1,5 @@
+n1 = int(input('Primeiro valor:'))
+n2 = int(input('Segundo valor:'))
+n3 = int(input('Terceiro valor:'))
+
+print(f'O maior número é {max(n1, n2, n3)} e o menor é {min(n1, n2, n3)}')
