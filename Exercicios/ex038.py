c81e3ed918f7df4ff6e929316fd345338db15821
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-06-06 18:24:50 -0300

    Comparando números
    Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
    - O primeiro valor é maior
    - O segundo valor é maior
    - Não existe valor maior, os dois são iguais.

---
+++ Exercicios/ex038.py
@@ -0,0 +1,11 @@
+numero1 = int(input('Primeiro número: '))
+numero2 = int(input('Segundo número: '))
+
+if numero1 > numero2:
+    print(f'O PRIMEIRO valor é o maior')
+
+elif numero2 > numero1:
+    print(f'O SEGUNDO valor é o maior')
+
+else:
+    print(f'Os dois valores são IGUAIS')
