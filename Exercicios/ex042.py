6e8b7e801bc1d08f7cff524a887d84e25570b944
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-06-10 12:11:32 -0300

    Analisando Triângulos v2.0
    Refaça o DESAFIO 035 dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado:
    - EQUILÁTERO: todos os lados iguais
    - ISÓSCELES: dois lados iguais, um diferente
    - ESCALENO: todos os lados diferentes.

---
+++ Exercicios/ex042.py
@@ -0,0 +1,18 @@
+s1 = float(input('PRIMEIRO segmento: '))
+s2 = float(input('SEGUNDO segmento: '))
+s3 = float(input('TERCEIRO segmento: '))
+
+if s1 < s2 + s3 and s2 < s1 + s3 and s3 < s1 + s2:
+    print('Os segmentos acima PODEM FORMAR um triângulo', end=' ')
+
+    if s1 == s2 == s3:
+        print('Equilátero')
+    
+    elif s1 != s2 != s3 != s1:
+        print('Escaleno')
+    
+    else:
+        print('Isósceles')
+
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR um triângulo !')
