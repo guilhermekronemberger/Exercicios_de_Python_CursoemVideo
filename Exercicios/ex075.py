648854d588bfd9794531ea13b853d72bcd3c6e11
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-07-15 21:49:49 -0300

    Análise de dados em uma Tupla
    Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final, mostre:
    A) Quantas vezes apareceu o valor 9.
    B) Em que posição foi digitado o primeiro valor 3.
    C) Quais foram os números pares.

--- Exercicios/ex075.py
+++ Exercicios/ex075.py
@@ -9,7 +9,7 @@ print(f'O valor 9 apareceu {num.count(9)} vezes')
 if 3 in num:
        print(f'O valor 3 apareceu na {num.index(3) + 1}º posição')
 else:
-     print('O valor 3 não foi digitado em nenhum posição')
+     print('O valor 3 não foi digitado em nenhuma posição')
 
 print('Os valores pares digitados foram: ', end='')
 for n in num:
