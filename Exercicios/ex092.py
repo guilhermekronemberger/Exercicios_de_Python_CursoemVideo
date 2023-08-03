a3f29b288a3fc90e1b3f29860681ac107396ac5a
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-08-02 23:25:02 -0300

    Cadastro de Trabalhador em Python
    Crie um programa que leia nome, ano de nascimento e carteira de trabalho e cadastre-o (com idade) em um dicionário. Se por acaso a CTPS for diferente de ZERO, o dicionário receberá também o ano de contratação e o salário. Calcule e acrescente, além da idade, com quantos anos a pessoa vai se aposentar.

---
+++ Exercicios/ex092.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+dados = {}
+
+dados['nome'] = str(input('Nome: ')).title()
+nascimento = int(input('Ano de Nascimento: '))
+dados['idade'] = datetime.now().year - nascimento
+dados['ctps'] = int(input('Cateira de trabalho (0 não tem): '))
+
+if dados['ctps'] != 0:
+    dados['contratação'] = int(input('Ano de Contratação: '))
+    dados['salário'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
+
+print('=' * 30)
+for k, v in dados.items():
+    print(f'- {k} tem o valor {v}')
