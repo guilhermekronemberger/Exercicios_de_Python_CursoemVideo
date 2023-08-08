e11ca0eb3717f9a8c2edba78225614e3720d0a29
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-08-08 11:31:26 -0300

    Função de Contador
    Faça um programa que tenha uma função chamada contador(), que receba três parâmetros: início, fim e passo. Seu programa tem que realizar três contagens através da função criada:
    a) de 1 até 10, de 1 em 1
    b) de 10 até 0, de 2 em 2
    c) uma contagem personalizada.

---
+++ Exercicios/ex098.py
@@ -0,0 +1,43 @@
+from time import sleep
+
+def contador(i, f, p):
+    if p < 0:
+        p *= -1
+
+    if p == 0:
+        p = 1
+
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    sleep(1)
+
+    if p < 0:
+        p *= -1
+
+    if p == 0:
+        p = 1
+
+    if i < f:
+        c = i 
+        while c <= f:
+            print(f'{c}', end=' ')
+            sleep(0.4)
+            c += p
+        print('FIM!')
+
+    else:
+        c = i
+        while c >= f:
+            print(f'{c}', end=' ')
+            sleep(0.4)
+            c -= p
+        print('FIM!')
+    print('=' * 40)
+
+contador(1, 10, 1)
+contador(10, 0, 2)
+
+print('Agora você pode personalizar a contagem!')
+inicio = int(input('Início: '))
+fim = int(input('Fim: '))
+passo = int(input('Passo: '))
+contador(inicio, fim, passo)
