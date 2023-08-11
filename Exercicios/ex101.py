5ae1746c5a00e452aa3966a6ddbea9e09e362ecf
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-08-11 17:21:26 -0300

    Funções para votação
    Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e OBRIGATÓRIO nas eleições.

--- Exercicios/ex101.py
+++ Exercicios/ex101.py
@@ -1,13 +1,12 @@
-from datetime import date
-
 def voto(ano):
+    from datetime import date
     ano_atual = date.today().year
     idade = ano_atual - ano
 
     if idade < 16:
-        return f"Com {idade} anos: Não vota!"
+        return f'Com {idade} anos: Não vota!'
     elif idade >= 16 and idade < 18 or idade > 65:
-        return f"Com {idade} anos: Voto opcional!"
+        return f'Com {idade} anos: Voto opcional!'
     else:
         return f'Com {idade} anos: Voto obrigatório!'
 
