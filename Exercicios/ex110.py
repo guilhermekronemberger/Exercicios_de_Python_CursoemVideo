34cf25c5b81da774554340fdb28a2c95998702ef
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-08-21 11:45:46 -0300

    Reduzindo ainda mais seu programa
    Adicione o módulo  criado nos desafios anteriores, uma função chamada resumo(), que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até aqui.

---
+++ Exercicios/ex110.py
@@ -0,0 +1,4 @@
+import ex110_m
+
+preco = float(input('Digite o preço: R$'))
+ex110_m.resumo(preco)
---
+++ Exercicios/ex110_m.py
@@ -0,0 +1,46 @@
+def aumentar(valor=0, taxa=0, format=False):
+    resposta = valor + (valor * taxa / 100) 
+    if format == False:
+        return resposta
+    else:
+        return moeda(resposta)
+
+
+def diminuir(valor=0, taxa=0, format=False):
+    resposta = valor - (valor * taxa / 100)
+    if format == False:
+        return resposta
+    else:
+        return moeda(resposta)
+
+
+def dobro(valor=0, format=False):
+    resposta = valor * 2 
+    if format == False:
+        return resposta
+    else:
+        return moeda(resposta)
+
+
+def metade(valor=0, format=False):
+    resposta = valor / 2
+    if format == False:
+        return resposta
+    else:
+        return moeda(resposta)
+
+
+def moeda(valor, moeda='R$'):
+    return f'{moeda}{valor:.2f}'.replace('.',',')
+
+
+def resumo(valor=0, taxaaumento=10, taxareduzida=10):
+    print('=' * 35)
+    print('-> RESUMO DO VALOR <- '.center(35))
+    print('=' * 35)
+    print(f'Valor analisado: \t{moeda(valor)}')
+    print(f'Dobro do preço: \t{dobro(valor, format=True)}')
+    print(f'Metade do preço: \t{metade(valor, format=True)}')
+    print(F'Com {taxaaumento}% de aumento: \t{aumentar(valor, taxareduzida, format=True)}')
+    print(F'Com {taxareduzida}% de redução: \t{diminuir(valor, taxaaumento, format=True)}')
+    print('=' * 35)
