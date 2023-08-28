9a004658e14f01f4cc4607e9432f362b3785a0f7
Guilherme Kronemberger <guilhermefkronemberger@gmail.com>
2023-08-28 08:29:45 -0300

    Criando um menu em Python
    Criar um menu em Python, usando modularização.

--- Exercicios/ex115.py
+++ Exercicios/ex115.py
@@ -3,15 +3,13 @@ from pacoteII.arquivo import *
 from time import sleep
 
 arq = 'listagem.txt'
-
 if not arquivoExiste(arq):
     criarArquivo(arq)
- 
 
 while True:
     resposta = menu(['Pessoas cadastradas', 'Novo cadastro', 'Sair'])
     if resposta == 1:
-        cabeçalho('Pessoas cadastradas')
+        lerArquivo(arq)
     elif resposta == 2:
         cabeçalho('Novo cadastro')
     elif resposta == 3:
--- Exercicios/pacoteII/arquivo/__init__.py
+++ Exercicios/pacoteII/arquivo/__init__.py
@@ -1,3 +1,5 @@
+from pacoteII.interface import *
+
 def arquivoExiste(nome):
     try:
         a = open(nome, 'rt')
@@ -16,3 +18,13 @@ def criarArquivo(nome):
         print('ERRO ao criar o arquivo!')
     else:
         print(f'Arquivo {nome} criado com sucesso!')
+
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('ERRO ao ler o arquivo!')
+    else:
+        cabeçalho('Pessoas cadastradas')
+        print(a.read())
